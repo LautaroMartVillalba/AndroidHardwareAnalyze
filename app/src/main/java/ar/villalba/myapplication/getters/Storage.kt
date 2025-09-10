33b6a6d6b68700c205f099217873ff6ae1d91563
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-09-10 00:26:23 -0300

    ADD: access variables and added used storage variable

--- app/src/main/java/ar/villalba/myapplication/getters/Storage.kt
+++ app/src/main/java/ar/villalba/myapplication/getters/Storage.kt
@@ -4,7 +4,9 @@ import android.os.Environment
 import android.os.StatFs
 
 class Storage {
-
+    val total = getTotalStorageInfo()
+    val rest = getRestStorageInfo()
+    val used = total - rest
 
     fun getTotalStorageInfo(): Long{
         val internalPath = Environment.getDataDirectory()
@@ -12,22 +14,19 @@ class Storage {
 
         val blockSize = stat.blockSizeLong
         val totalBlocks = stat.blockCountLong
-        val availableBlocks = stat.availableBlocksLong
 
         val totalSpaceBytes = totalBlocks * blockSize / 1024 / 1024
-        val availableSpaceBytes = availableBlocks * blockSize
 
         return totalSpaceBytes
     }
+
     fun getRestStorageInfo(): Long{
         val internalPath = Environment.getDataDirectory()
         val stat = StatFs(internalPath.path)
 
         val blockSize = stat.blockSizeLong
-        val totalBlocks = stat.blockCountLong
         val availableBlocks = stat.availableBlocksLong
 
-        val totalSpaceBytes = totalBlocks * blockSize
         val availableSpaceBytes = availableBlocks * blockSize / 1024 / 1024
 
         return availableSpaceBytes
