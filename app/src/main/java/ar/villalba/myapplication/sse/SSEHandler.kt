eb1892c75d96f68511cbb9052ac7dcba86494ce0
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-09-27 16:52:10 -0300

    DELETE: unused classes in API flow execution

--- app/src/main/java/ar/villalba/myapplication/sse/SSEHandler.kt
+++ app/src/main/java/ar/villalba/myapplication/sse/SSEHandler.kt
@@ -1,12 +1,12 @@
-package ar.villalba.myapplication.sse
-
-import com.launchdarkly.eventsource.MessageEvent
-
-interface SSEHandler {
-
-    fun onSSEConnectionOpened()
-    fun onSSEConnectionClosed()
-    fun onSSEEventReceived(event: String?, messageEvent: MessageEvent?)
-    fun onSSEError(t: Throwable?)
-
-}
+//package ar.villalba.myapplication.sse
+//
+//import com.launchdarkly.eventsource.MessageEvent
+//
+//interface SSEHandler {
+//
+//    fun onSSEConnectionOpened()
+//    fun onSSEConnectionClosed()
+////    fun onSSEEventReceived(event: String?, messageEvent: MessageEvent?)
+//    fun onSSEError(t: Throwable?)
+//
+//}
