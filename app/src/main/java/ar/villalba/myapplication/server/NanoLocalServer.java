4fd72008ac1d33b077eb9633a453ebfb25423b9a
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-10-02 22:54:18 -0300

    REFACTOR: remove -sse and -static suffixes from API endpoints ADD: NanoLocalServer: withCORS function to add CORS headers to responses NanoLocalServer: logic to serve Swagger UI and openapi.json documentation

--- app/src/main/java/ar/villalba/myapplication/server/NanoLocalServer.java
+++ app/src/main/java/ar/villalba/myapplication/server/NanoLocalServer.java
@@ -1,10 +1,23 @@
 package ar.villalba.myapplication.server;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Build;
 import android.util.Log;
+
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Supplier;
 import ar.villalba.myapplication.getters.JSONManager;
 import ar.villalba.myapplication.getters.SensorInDevice;
@@ -30,7 +43,12 @@ public class NanoLocalServer extends NanoHTTPD {
                                 "Something went wrong. Try again later or contact support.");
         }
     }
-
+    private Response withCORS(Response resp) {
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        resp.addHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
+        return resp;
+    }
 
     @Override
     public Response serve(IHTTPSession session) {
@@ -40,46 +58,116 @@ public class NanoLocalServer extends NanoHTTPD {
         sensorInDevice.start();
 
         switch (uri){
-            case "/api/" + API_VERSION + "/hardware/cpu-sse":
+            case "/api/" + API_VERSION + "/hardware/cpu":
                     return streamSafeResponseAbstraction(jsonManager::CPUJson);
 
-            case "/api/" + API_VERSION + "/hardware/ram-sse":
+            case "/api/" + API_VERSION + "/hardware/ram":
                     return streamSafeResponseAbstraction(jsonManager::ramJson);
 
-            case "/api/" + API_VERSION + "/hardware/battery-sse":
+            case "/api/" + API_VERSION + "/hardware/battery":
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
                     return streamSafeResponseAbstraction(jsonManager::batteryJson);
                 }
 
-            case "/api/" + API_VERSION + "/hardware/sensors-sse":
+            case "/api/" + API_VERSION + "/hardware/sensors":
                 return ServiceController.sensorStream(sensorInDevice);
 
+            case "/api/" + API_VERSION + "/hardware/device":
+                    return newFixedLengthResponse(
+                            Response.Status.OK,
+                            "application/json",
+                            jsonManager.deviceJson().toString()
+                    );
 
-            case "/api/" + API_VERSION + "/hardware/device-static":
-                return newFixedLengthResponse(Response.Status.OK, "application/json",
-                        jsonManager.deviceJson().toString());
 
-            case "/api/" + API_VERSION + "/hardware/screen-static":
+            case "/api/" + API_VERSION + "/hardware/screen":
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    return newFixedLengthResponse(Response.Status.OK, "application/json",
-                            jsonManager.screenJson().toString());
+                    return newFixedLengthResponse(
+                            Response.Status.OK,
+                            "application/json",
+                            jsonManager.screenJson().toString()
+                    );
                 } else {
-                    return newFixedLengthResponse(Response.Status.NOT_IMPLEMENTED, "text/plain",
-                            "Service not yet available in Android " + Build.VERSION.SDK_INT + ".");
+                    return withCORS(newFixedLengthResponse(
+                            Response.Status.NOT_IMPLEMENTED,
+                            "text/plain",
+                            "Service not yet available in Android " + Build.VERSION.SDK_INT + ".")
+                    );
                 }
 
-            case "/api/" + API_VERSION + "/hardware/storage-static":
+            case "/api/" + API_VERSION + "/hardware/storage":
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    return newFixedLengthResponse(Response.Status.OK, "application/json",
-                            jsonManager.storageJson().toString());
+                        return newFixedLengthResponse(
+                                Response.Status.OK,
+                                "application/json",
+                                jsonManager.storageJson().toString()
+                        );
                 } else {
-                    return newFixedLengthResponse(Response.Status.NOT_IMPLEMENTED, "text/plain",
-                            "Service not yet available in Android " + Build.VERSION.SDK_INT + ".");
+                    return newFixedLengthResponse(
+                            Response.Status.NOT_IMPLEMENTED,
+                            "text/plain",
+                            "Service not yet available in Android " + Build.VERSION.SDK_INT + "."
+                    );
                 }
+
+            case "/swagger/index.html":
+                return serveAsset("swagger-ui/index.html", "text/html");
+
+            case "/swagger/swagger-ui.css":
+                return serveAsset("swagger-ui/swagger-ui.css", "text/css");
+
+            case "/swagger/swagger-ui-bundle.js":
+                return serveAsset("swagger-ui/swagger-ui-bundle.js", "application/javascript");
+
+            case "/openapi.json":
+                Response resp;
+
+                resp = newFixedLengthResponse(Response.Status.OK, "application/json", obtainOpenApiJson());
+                resp.addHeader("Access-Control-Allow-Origin", "*");
+                resp.addHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+
+                return withCORS(resp);
 
             default:
                 return newFixedLengthResponse(Response.Status.NOT_FOUND, "text/plain",
                         "Endpoint not found");
+        }
+    }
+
+    private String obtainOpenApiJson(){
+        InputStream is;
+        try {
+            is = localContext.getAssets().open("openapi.json");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while (true) {
+            try {
+                if ((line = reader.readLine()) == null) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            sb.append(line);
+        }
+        try {
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sb.toString();
+    }
+
+
+    private Response serveAsset(String assetPath, String contentType) {
+        try {
+            AssetManager am = localContext.getAssets();
+            InputStream is = am.open(assetPath);
+            return newChunkedResponse(Response.Status.OK, contentType, is);
+        } catch (IOException e) {
+            return newFixedLengthResponse(Response.Status.NOT_FOUND, "text/plain", "Not found");
         }
     }
 
